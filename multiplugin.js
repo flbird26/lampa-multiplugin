48b3d64a4bb5f1c8d2e9dd8b7a0f76c9a69ef85a
flbird26 <94467132+flbird26@users.noreply.github.com>
2026-01-08 13:49:03 +0200

    Update multiplugin.js

--- multiplugin.js
+++ multiplugin.js
@@ -1,9 +1,5 @@
-(function() {
-    GM_xmlhttpRequest({
-        method: "GET",
-        url: "https://dl.dropboxusercontent.com/scl/fi/w7ptfh525zesvinbqtxja/multiplugin.js?rlkey=1zozlazpsovf4sy96pdez6d23&st=544lfp8b",
-        onload: function(response) {
-            eval(response.responseText);
-        }
-    });
+(function () {
+    'use strict';
+    Lampa.Utils.putScriptAsync(['https://dl.dropboxusercontent.com/scl/fi/w7ptfh525zesvinbqtxja/multiplugin.js?rlkey=1zozlazpsovf4sy96pdez6d23&st=544lfp8b'], function () { });
+
 })();
