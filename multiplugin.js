a0b2f9e5e6eb66fea57500d96aa86b9bac8e1361
flbird26 <94467132+flbird26@users.noreply.github.com>
2026-01-08 14:15:59 +0200

    Delete multiplugin.js

--- multiplugin.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-    Lampa.Utils.putScriptAsync([
-        'https://dl.dropboxusercontent.com/scl/fi/w7ptfh525zesvinbqtxja/multiplugin.js?rlkey=1zozlazpsovf4sy96pdez6d23&st=544lfp8b',
-        'https://dl.dropboxusercontent.com/scl/fi/1iqq9u75lsjta1h8q3o8f/plugins_list.js?rlkey=jhttw50k2zxx76nygh3ua16ac&st=n99lsokd'
-    ], function () { });
-})();
