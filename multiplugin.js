0634601b81aa9a923ca0643037f135bcadcc6340
flbird26 <94467132+flbird26@users.noreply.github.com>
2026-01-08 14:12:04 +0200

    Update multiplugin.js

--- multiplugin.js
+++ multiplugin.js
@@ -1,5 +1,7 @@
 (function () {
     'use strict';
-    Lampa.Utils.putScriptAsync(['https://dl.dropboxusercontent.com/scl/fi/w7ptfh525zesvinbqtxja/multiplugin.js?rlkey=1zozlazpsovf4sy96pdez6d23&st=544lfp8b'], function () { });
-
+    Lampa.Utils.putScriptAsync([
+        'https://dl.dropboxusercontent.com/scl/fi/w7ptfh525zesvinbqtxja/multiplugin.js?rlkey=1zozlazpsovf4sy96pdez6d23&st=544lfp8b',
+        'https://dl.dropboxusercontent.com/scl/fi/1iqq9u75lsjta1h8q3o8f/plugins_list.js?rlkey=jhttw50k2zxx76nygh3ua16ac&st=n99lsokd'
+    ], function () { });
 })();
